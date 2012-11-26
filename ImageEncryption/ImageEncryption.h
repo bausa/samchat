65e20cd1ce13305961b3515952847ed4507dd27a
Sam Baumgarten <sbaumgarten@makrr.com>
2012-11-26 10:33:10 -0800

    Initial Commit

---
+++ ImageEncryption/ImageEncryption.h
@@ -0,0 +1,13 @@
+//
+//  ImageEncryption.h
+//  ImageEncryption
+//
+//  Created by Sam Baumgarten on 11/26/12.
+//  Copyright (c) 2012 Makrr. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ImageEncryption : NSObject
+
+@end
---
+++ ImageEncryption/ImageEncryption.m
@@ -0,0 +1,13 @@
+//
+//  ImageEncryption.m
+//  ImageEncryption
+//
+//  Created by Sam Baumgarten on 11/26/12.
+//  Copyright (c) 2012 Makrr. All rights reserved.
+//
+
+#import "ImageEncryption.h"
+
+@implementation ImageEncryption
+
+@end
